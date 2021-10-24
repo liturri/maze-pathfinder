4a56ee2f203971330f03496900b03be9990cb133
Lionel Iturri <liturri@gmail.com>
2021-10-24 00:49:03 -0300

    Fix cantItem / cantCpu Remainder no exact division

--- src/pf.cc
+++ src/pf.cc
@@ -8,6 +8,9 @@
 #include "maze.hxx"
 #include "seeds.hxx"
 #include <thread>
+#include <mutex>
+
+std::mutex console;
 
 MazeLengthsDB lengthsDB;
 
@@ -67,7 +70,9 @@ TypeLength CreateMaze(TypeSeed seed, int lines, int columns, int heuristic)
 
 void workThread(TypeSeed from, TypeSeed to)
 {
-   // std::cerr << "Work: " << std::this_thread::get_id() << std::endl;
+   console.lock();
+   std::cerr << "Thread: " << std::this_thread::get_id() << " From: " << from << " To: " << to << std::endl;
+   console.unlock();
    for (TypeSeed s = from; s < to; s++)
    {
       Benchmark b;
@@ -75,7 +80,9 @@ void workThread(TypeSeed from, TypeSeed to)
       if (lengthsDB.IsEmpty(length))
       {
          lengthsDB.AddItem(length, s);
+         console.lock();
          std::cout << "Len: " << length << " Seed: " << s << std::endl;
+         console.unlock();
       }
       // lengthsDB.IsEmpty(s, length);
       // std::cerr << "Len: " << length << " Seed: " << s << " Time: " << b.elapsed() << std::endl;
@@ -102,11 +109,13 @@ int main(int argc, char *argv[])
 
    std::vector<std::thread> threadList;
 
-   for (unsigned int threadNumber = 0; threadNumber < processorsCount; threadNumber++)
+   for (unsigned int threadNumber = 0; threadNumber < processorsCount - 1; threadNumber++)
    {
       std::thread th = std::thread(workThread, seed + itemsPerThread * threadNumber, seed + itemsPerThread * (threadNumber + 1));
       threadList.push_back(std::move(th));
    }
+   std::thread th = std::thread(workThread, seed + itemsPerThread * (processorsCount - 1), seed + totalCantItems);
+   threadList.push_back(std::move(th));
 
    for (std::thread &th : threadList)
    {
@@ -114,7 +123,9 @@ int main(int argc, char *argv[])
       if (th.joinable())
          th.join();
    }
+   console.lock();
    std::cerr << "Seeds: " << seed << '-' << seed + totalCantItems << "  time: " << b.elapsed() << std::endl;
+   console.unlock();
    {
       auto fileFD = std::ifstream(seedsFileName);
       lengthsDB.ImportTextFromFile(fileFD);
